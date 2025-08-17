710162a38daefd684b43b84151d9280884c4f482
rajesh <rajesh.r@ndimensionz.com>
2025-08-17 14:36:12 +0530

    change destory pr

--- infra/nlb/locals.tf
+++ infra/nlb/locals.tf
@@ -36,13 +36,18 @@ locals {
 
   target_az = data.terraform_remote_state.vpc.outputs.ec2_az
   unique_subnets = [
-  for az in keys(local.az_to_subnet_ids) : local.az_to_subnet_ids[az][0]
-]
+    for az in keys(local.az_to_subnet_ids) : local.az_to_subnet_ids[az][0]
+  ]
 
-subnet_mapping = [
-  for subnet_id in local.unique_subnets : {
-    subnet_id = subnet_id
-  }
-]
+  subnet_mapping = [
+    for subnet_id in local.unique_subnets : {
+      subnet_id = subnet_id
+    }
+  ]
 
+}
+
+locals {
+  nlb_dns_name         = try(data.terraform_remote_state.nlb.outputs.nlb_dns_name, data.terraform_remote_state.nlb.outputs.lb_dns_name)
+  vpc_link_target_arns = [try(data.terraform_remote_state.nlb.outputs.nlb_arn, data.terraform_remote_state.nlb.outputs.lb_arn)]
 }
