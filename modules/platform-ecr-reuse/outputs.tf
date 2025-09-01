83a99223615b3c2738ebe313bc7a365be6c15c5b
rajesh <rajesh.r@ndimensionz.com>
2025-09-01 16:53:42 +0530

    update

--- modules/platform-ecr-reuse/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "repository_names" {
-  value = local.repository_names
-}
-output "repository_urls" {
-  value = local.repository_urls
-}
-output "repository_arns" {
-  value = local.repository_arns
-}
