543a917427c9225649c8d636a1971711dfadeb92
rajesh <rajesh.r@ndimensionz.com>
2025-09-01 16:34:06 +0530

    ecr changes

--- modules/platform-ecr-reuse/main.tf
+++ modules/platform-ecr-reuse/main.tf
@@ -1,6 +1,4 @@
-terraform {
-  required_version = ">= 1.5.0"
-}
+terraform { required_version = ">= 1.5.0" }
 
 data "terraform_remote_state" "ecr" {
   backend = "s3"
@@ -13,6 +11,6 @@ data "terraform_remote_state" "ecr" {
 
 locals {
   repository_names = try(data.terraform_remote_state.ecr.outputs.repository_names, [])
-  repository_urls  = try(data.terraform_remote_state.ecr.outputs.repository_urls,  [])
-  repository_arns  = try(data.terraform_remote_state.ecr.outputs.repository_arns,  [])
+  repository_urls  = try(data.terraform_remote_state.ecr.outputs.repository_urls, [])
+  repository_arns  = try(data.terraform_remote_state.ecr.outputs.repository_arns, [])
 }
