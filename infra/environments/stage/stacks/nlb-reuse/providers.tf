103d920f6e23e48bdff7223c32a8fdd7ae06adbe
rajesh <rajesh.r@ndimensionz.com>
2025-09-01 15:25:34 +0530

    fix(nlb-reuse): hard-reset to pure remote-state reader (remove any check/precondition)

--- infra/environments/stage/stacks/nlb-reuse/providers.tf
+++ infra/environments/stage/stacks/nlb-reuse/providers.tf
@@ -4,6 +4,4 @@ terraform {
     aws = { source = "hashicorp/aws", version = ">= 5.0" }
   }
 }
-provider "aws" {
-  region = var.region
-}
+provider "aws" { region = var.region }
--- modules/platform-nlb-reuse/variables.tf
+++ modules/platform-nlb-reuse/variables.tf
@@ -1,9 +1,3 @@
-variable "platform_state_bucket" {
-  type = string
-}
-variable "platform_state_region" {
-  type = string
-}
-variable "platform_nlb_state_key" {
-  type = string
-}
+variable "platform_state_bucket" { type = string }
+variable "platform_state_region" { type = string }
+variable "platform_nlb_state_key" { type = string }
