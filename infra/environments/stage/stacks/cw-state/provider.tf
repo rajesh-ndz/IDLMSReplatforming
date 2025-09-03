327301179de6de61856d96218e8e17365e07c211
rajesh <rajesh.r@ndimensionz.com>
2025-09-03 10:41:21 +0530

    added destroy yml

--- infra/environments/stage/stacks/cw-state/provider.tf
+++ infra/environments/stage/stacks/cw-state/provider.tf
@@ -4,5 +4,5 @@ terraform {
     aws = { source = "hashicorp/aws", version = ">= 5.0" }
   }
 }
-variable "region" { type = string }
+
 provider "aws" { region = var.region }
--- infra/environments/stage/stacks/cw-state/variables.tf
+++ infra/environments/stage/stacks/cw-state/variables.tf
@@ -1,3 +1,4 @@
 variable "state_bucket" { type = string }
-variable "state_key"    { type = string }
+variable "state_key" { type = string }
 variable "state_region" { type = string }
+variable "region" { type = string }
