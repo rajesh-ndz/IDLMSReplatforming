6f8c9fec9e8b5090f6de9362890c9d3a1047a180
rajesh <rajesh.r@ndimensionz.com>
2025-09-03 11:25:35 +0530

    chore: flatten infra tree (cloudwatch, rest-api, ssm, network, compute, ecr, nlb)

--- infra/platform/app/ssm_publish/variables.tf
+++ infra/platform/app/ssm_publish/variables.tf
@@ -1,7 +1,4 @@
-variable "region" { type = string }
+variable "region"      { type = string }
 variable "path_prefix" { type = string }
-variable "values" { type = map(string) }
-variable "overwrite" {
-  type    = bool
-  default = true
-}
+variable "values"      { type = map(string) }
+variable "overwrite"   { type = bool default = true }
