7905f9bbae768120bf1041ddf44e1eef56faca7a
rajesh <rajesh.r@ndimensionz.com>
2025-09-06 22:29:26 +0530

    feat: reuse platform-main resources in IDLMS

---
+++ infra/platform-remote/variables.tf
@@ -0,0 +1,4 @@
+variable "env_name"                { type = string }
+variable "region"                  { type = string }
+variable "platform_state_bucket"   { type = string }
+variable "platform_state_region"   { type = string }
